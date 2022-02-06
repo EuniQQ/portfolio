3f02b9dae9ce893056d4b5a07c09e5ff7cf7eb9c
unknown <eunice911qq@gmail.com>
2022-02-06 23:12:24 +0800

    嘗試以jquery方式點擊變換後台配色

--- backend.php
+++ backend.php
@@ -19,7 +19,6 @@ if(!isset($_SESSION['login'])){
     <link href="./css/css.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> -->
     <script src="./js/jquery-1.9.1.min.js"></script>
     <script src="./js/js.js"></script>
 	<style>
@@ -35,6 +34,7 @@ if(!isset($_SESSION['login'])){
 
         body{
             background-color:white;
+            margin:0;
         }
 	</style>
 </head>
@@ -115,15 +115,29 @@ if(!isset($_SESSION['login'])){
             </div>
             <div style="clear:both;"></div>
             <div
-                class="footer">
-                <span class=" text-center t" style="line-height:80px;">&copy; copyright by Eunice Chang</span>
-                
+            class="footer">
+                <span class="colorBtn brown" style="background-color:#999900;"></span>
+                <span class="colorBtn purple" style="background-color:rgb(124, 94, 123);"></span>
+                <span class="colorBtn blue" style="background-color:rgb(74, 126, 161);"></span>
+                <!-- <span class=" text-center t" style="line-height:80px;display: inline-block;">&copy; copyright by Eunice Chang</span> -->
+               
             </div>
         </div>
 
-<!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script> -->
+       <script>
+           $(document).ready(function(){
+               $('.brown').click(function(){
+                    $('.ti').css('background-color','#999900');  //標題列
+                    $('.yel td').css('background-color','#EEE8AA'); //分頁欄位
+                    // $('.yel td').css('background-color','#BDB76B'); //分頁欄位
+                    $('.footer').css('background-color','#CCCC99'); //頁尾
+                    $('.dbor').css('background-color','	#FFFAFA'); //選單列
+                    });
+
+                // $('.blue').click(function(){
+                //     $('.ti').css('background-color','rgb(74, 126, 161)');  
+           }
+       </script>
 </body>
 
 </html>
