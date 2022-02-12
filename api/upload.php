8532641c272aff8b840773c481e5738e55c26524
unknown <eunice911qq@gmail.com>
2022-02-12 15:43:30 +0800

    修正完成更新圖片功能

--- api/upload.php
+++ api/upload.php
@@ -1,17 +1,39 @@
 <?php
 include_once "../base.php";
 
-if(!empty($_FILES['img']['tmp_name'])){
-    move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
+ 
+switch($DB->table){ 
+case "graphic": 
+    if(!empty($_FILES['img']['tmp_name'] || $_FILES['img_sm']['tmp_name'] )){
+        move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
+        move_uploaded_file($_FILES['img_sm']['tmp_name'],"../img/".$_FILES['img']['name']);
+        //先取出該筆資料
+        $data=$DB->find($_POST['id']);
+    
+        //更新img欄位的內容
+        $data['img']=$_FILES['img']['name'];
+        $data['img_sm']=$_FILES['img_sm']['name'];
+        
+        //資料寫入資料表
+        $DB->save($data);
+    }
+   
+    break;
+    default:  
+              
 
-    //先取出該筆資料
-    $data=$DB->find($_POST['id']);
+    if(!empty($_FILES['img']['tmp_name'])){
+        move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
 
-    //更新img欄位的內容
-    $data['img']=$_FILES['img']['name'];
+        //先取出該筆資料
+        $data=$DB->find($_POST['id']);
 
-    //資料寫入資料表
-    $DB->save($data);
+        //更新img欄位的內容
+        $data['img']=$_FILES['img']['name'];
+
+        //資料寫入資料表
+        $DB->save($data);
+    }
 }
 
 to("../backend.php?do=".$DB->table);
--- modal/upload.php
+++ modal/upload.php
@@ -2,25 +2,33 @@
 
 <h3><?=$DB->upload;?></h3>
 <hr>
-                              <!-- 有上傳檔案就需要打enctype="multipart/-form-data -->
-<form action="api/upload.php?do=<?=$DB->table;?>" method="post" enctype="multipart/-form-data">    
+                              <!-- 有上傳檔案就需要打enctype="multipart/form-data -->
+<form action="api/upload.php?do=<?=$DB->table;?>" method="post" enctype="multipart/form-data">    
 
     <table>
         <tr>
             <?php 
             switch($DB->table){ 
             case "graphic": 
-            echo "<td>更新縮圖：</td>";
-            echo "<td><input type='file' name='img_sm'></td>"; 
-            echo "<td>更新原圖：</td>";
-            echo "<td><input type='file' name='img'></td>";
-            break;
-            default:    
-            echo "<td>$DB->upload：</td>";
-            echo "<td><input type='file' name='img'></td>";
-            //    name欄位跟資料表是一致的
-        }
             ?>
+
+            <td>更新縮圖：</td>";
+            <td><input type='file' name='img_sm'></td>"; 
+            <td>更新原圖：</td>";
+            <td><input type='file' name='img'></td>";
+
+            <?php
+            break;
+            default:  
+            ?> 
+
+            <td><?=$DB->upload?>：</td>
+            <td><input type='file' name='img'></td>
+            <!-- name欄位跟資料表是一致的 -->
+        <?php
+        }
+        ?>
+            
         </tr>
         
     </table>
